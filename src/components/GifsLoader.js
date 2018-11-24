8dbc5a80ea6e7f01aad439c6cf02dd329f2d97da
DanielChen1234 <dchen8492@yahoo.com>
2018-11-23 22:10:53 -0500

    titles now appear on the gif

--- src/components/GifsLoader.js
+++ src/components/GifsLoader.js
@@ -1,10 +1,7 @@
 import React, { Component } from 'react'
 import {GridList, GridTile} from 'material-ui/GridList'
 
-
-
 export default class GifsLoader extends Component {
-
   render() {
 
     const gifs = this.props.gifs
@@ -12,15 +9,18 @@ export default class GifsLoader extends Component {
     if (gifs){
       return <GridList cols={3}>
                 {gifs.map(gif => (
-                  <GridTile key={gif.id}>
+                  <GridTile 
+                  key={gif.id}
+                  title={gif.title}
+                  >
                     <img src={gif.images.downsized_large.url} />         
                   </GridTile>
                 ))}
               </GridList>
     } else {
       return <GridList cols={1}>
-                <GridTile key={'52qtwCtj9OLTi'}>
-                  <img src={'https://media.giphy.com/media/52qtwCtj9OLTi/giphy.gif'} />
+                <GridTile key={'RqFXe34guTAAg'}>
+                  <img src={'https://media.giphy.com/media/RqFXe34guTAAg/giphy.gif'} />
                 </GridTile>
             </GridList>
     }
