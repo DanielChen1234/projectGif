44ef0d5df6755c6d5ef4c999b8f3db643ea41cfd
DanielChen1234 <dchen8492@yahoo.com>
2018-11-26 14:53:27 -0500

    Updates

---
+++ precache-manifest.905f286a067b10570cf0d660c200a93b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ac02e368cd804999d171",
+    "url": "/projectGif/static/js/runtime~main.ac02e368.js"
+  },
+  {
+    "revision": "a027790079c967a99995",
+    "url": "/projectGif/static/js/main.a0277900.chunk.js"
+  },
+  {
+    "revision": "1e7a89b8c7a6a7a0039f",
+    "url": "/projectGif/static/js/1.1e7a89b8.chunk.js"
+  },
+  {
+    "revision": "a027790079c967a99995",
+    "url": "/projectGif/static/css/main.7c479edc.chunk.css"
+  },
+  {
+    "revision": "46cba02f818cfb754af1adfc643a64fc",
+    "url": "/projectGif/index.html"
+  }
+];
